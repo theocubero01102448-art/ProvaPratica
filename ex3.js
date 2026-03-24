f8f8fa13bb28461271e340e03fa6afc39656e4e7
Theo Shinigami <theo.cubero01102448@sesisenaipr.org.br>
2026-03-24 10:50:34 -0300

    provapratica

---
+++ ex3.js
@@ -0,0 +1,6 @@
+let idade = 16;
+if (idade >= 18) {
+    console.log("Maior de idade");
+} else {
+    console.log("Menor de idade");
+}
---
+++ ex4.js
@@ -0,0 +1,9 @@
+//criar uma variável nota e mostrar
+let nota = 5
+if(nota >= 7) {
+    console.log("Aprovado");
+} else if(nota = 5) {
+    console.log("Recuperação");
+} else if(nota < 5) {
+    console.log("Reprovado");
+}
